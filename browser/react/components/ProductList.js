e2d226a6868cd322171ae986f1b46bbcf0f5a33c
Wasif Zaman <wasifzaman@Wasifs-MacBook-Pro.local>
2017-09-16 15:14:13 -0400

    emptied

--- browser/react/components/ProductList.js
+++ browser/react/components/ProductList.js
@@ -1,23 +1,7 @@
 import React from 'react'
-import ProductForm from './ProductForm'
 
 const ProductList = (props) => {
-  const { products, onDeleteHandler, onSaveHandler } = props
 
-  return (
-    <div>
-    {
-      products.map(product=> (
-        <div className='col-4 col-md-4' key={ product.id }>
-          <ProductForm
-            product={product}
-            onSaveHandler={ onSaveHandler }
-            onDeleteHandler={ onDeleteHandler }/>
-        </div>
-      ))
-    }
-    </div>
-  )
 }
 
 export default ProductList
